a22878b0ea357ba8e0848c089fd9001637ae80ad
87309 <45799141+87309@users.noreply.github.com>
2019-01-04 14:18:35 -0500

    turn in
    0

--- team0.py
+++ team0.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,18 +7,31 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'team0' # Only 10 chars displayed.
+strategy_name = 'Collude first 100 rounds unless betrayed. Betray 11th round forward.'
+strategy_description = '''\
+Betray if ever betrayed.
+If I haven't been betrayed yet, I'll betray starting with the 10th round.
+'''
+
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
     '''
-
+    # If the other player has betrayed or this is the last half of the game, 
+    if 'b' in their_history or len(their_history)>10: 
+        return 'b'               # Betray.
+    else:
+        return 'c'         # but 90% of the time collude
+     
+    
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
